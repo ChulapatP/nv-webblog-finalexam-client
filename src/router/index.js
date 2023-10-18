6fd7342ccbabfb587400b823071467e8f26fc55f
ChulapatP <1137322chulapat@gmail.com>
2023-10-18 14:17:29 +0700

    fisrt

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import UserIndex from '@/components/Users/Index'
-import UserEdit from '@/components/Users/EditUser'
-import UserShow from '@/components/Users/ShowUser'
-import UserCreate from '@/components/Users/CreateUser'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/user/create',
-      name: 'user-create',
-      component: UserCreate
-    },
-    {
-      path: '/user/edit/:userId',
-      name: 'user-edit',
-      component: UserEdit
-    },
-    {
-      path: '/user/:userId',
-      name: 'user',
-      component: UserShow
-    },
-    {
-      path: '/users',
-      name: 'users',
-      component: UserIndex
-    },
-
-    
-
+      path: '/user',
+      name: '/user',
+      component: HelloWorld
+    }
   ]
 })
